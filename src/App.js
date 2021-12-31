69c25b994e9ff285edea62a63b83f69d678716bb
ACaruso <alequis@gmail.com>
2021-12-31 18:43:28 -0300

    feat: simple read operation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import Container from "@mui/material/Container";
 import Header from "./Header/header";
 import DogTable from "./DogTable/dog-table";
 
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref } from "firebase/database";
+import { firebaseConfig } from "../firebase.config";
+
+const app = initializeApp(firebaseConfig);
+export const dbRef = ref(getDatabase(app));
+
 const App = () => ( 
     <Container>
         <Header />
