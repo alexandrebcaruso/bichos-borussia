e5a42ae3ec0e1b348336adea8eeb37c76b7b7c91
ACaruso <alequis@gmail.com>
2022-03-29 11:41:45 -0300

    feat: add service worker

--- src/index.js
+++ src/index.js
@@ -3,3 +3,23 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+
+const registerServiceWorker = () => {
+    if ('serviceWorker' in navigator) {
+        try {
+            const registration = navigator.serviceWorker.register('./service-worker.js');
+            if (registration.installing) {
+                console.log('Service worker installing');
+            } else if (registration.waiting) {
+                console.log('Service worker installed');
+            } else if (registration.active) {
+                console.log('Service worker active');
+            }
+        } catch (error) {
+            console.error(`Registration failed with ${error}`);
+        }
+    }
+};
+
+
+registerServiceWorker();
--- webpack.common.js
+++ webpack.common.js
@@ -49,7 +49,8 @@ module.exports = {
         new CopyPlugin({
             patterns: [
                 { from: "./public/icons/", to: "./icons/" },
-                { from: "./public/manifest.json", to: "./manifest.json",}
+                { from: "./public/manifest.json", to: "./manifest.json" },
+                { from: "./src/service-worker.js", to: "./service-worker.js" },
             ],
         }),
     ],
