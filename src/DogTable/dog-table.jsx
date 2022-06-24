5bee49b4929c3329e2eb940a491110113de028f8
ACaruso <alequis@gmail.com>
2022-06-24 11:42:43 -0300

    chore: rm react imports (new jsx transform)

--- src/DogTable/dog-table.jsx
+++ src/DogTable/dog-table.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { firebaseApp } from "../App";
 import { getDatabase, ref, onValue, set, update } from "firebase/database";
