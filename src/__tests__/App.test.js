0ed18ea113151cc93f5977e03d3801a60c4bdf2a
ACaruso <alequis@gmail.com>
2022-06-29 12:57:06 -0300

    chore: to jsx

--- src/__tests__/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen} from "@testing-library/react";
-import "@testing-library/jest-dom";
-import App from "../App";
-
-test("renders the screen", () => {
-    render(<App />);
-    const element = screen.getByTestId("app");
-    expect(element).toBeInTheDocument();
-});
